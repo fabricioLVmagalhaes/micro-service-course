632562e3e9968b3eeec5f525667ff8b3e424f05d
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2021-04-08 07:04:49 -0300

    Project hr-oauth created

---
+++ hr-oauth/src/test/java/com/fabricio/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabricio.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
